05c604c3b0fd093f784dd52fd8ada65cf8e56d83
Harsh Srivastava <harshsri170995@outlook.com>
2016-07-03 10:47:50 +0530

    Add project files.

---
+++ KLanguage/KEnv.cs
@@ -0,0 +1,97 @@
+﻿using Infinity.Grammar.Parsing;
+using Infinity.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KLanguage
+{
+    public class KEnv
+    {
+        public KEnv()
+        {
+            
+        }
+
+        public void Interpret(ParseTreeNode ptn)
+        {
+            switch(ptn.Name)
+            {
+                case "PROGRAM":
+                    Interpret((ParseTreeNode)ptn.Children[1]);
+                    break;
+                case "CODE":
+                    ParseTreeNode node = (ParseTreeNode)ptn.Children[0];
+                    foreach(ParseTreeNode p in node.Children)
+                    {
+                        Interpret(p);
+                    }
+                    break;
+                case "STMT":
+                    Interpret((ParseTreeNode)ptn.Children[0]);
+                    break;
+                case "DECLR":
+                    string number = ((ParseTreeNode)ptn.Children[1]).InternalValue.Substring(2);
+                    number = number.Replace('k', '0').Replace('K', '1');
+                    int idx = Convert.ToInt32(number, 2);
+                    if (!VariableEngine.GetPool("/").HasVariableWithName("var" + idx))
+                    {
+                        VariableEngine.Put("/var" + idx, new Variable(0, "/Integer"));
+                    }
+                    else
+                    {
+                        Console.WriteLine("ERROR: Already DECLARED 0x"+number);
+                    }
+                    break;
+                case "PRINT":
+                    ParseTreeNode data = (ParseTreeNode)ptn.Children[1];
+                    foreach(ParseTreeNode p in data.Children)
+                    {
+                        number = p.InternalValue;
+                        number = number.Replace('k', '0').Replace('K', '1');
+                        idx = Convert.ToInt32(number, 2);
+                        if (idx > 255)
+                        {
+                            int var = idx % 256;
+                            if (VariableEngine.GetPool("/").HasVariableWithName("var" + var))
+                            {
+                                Variable v = VariableEngine.Get("/var" + var);
+                                Console.Write(v.Value);
+                            }
+                            else
+                            {
+                                Console.Write("[ERROR:0x" + number + " NOT FOUND]");
+                            }
+                        }
+                        else
+                        {
+                            Console.Write((char)idx);
+                        }
+                    }
+                    Console.Write("\n");
+                    break;
+                case "INPUT":
+                    number = ((ParseTreeNode)ptn.Children[1]).InternalValue;
+                    number = number.Replace('k', '0').Replace('K', '1');
+                    idx = Convert.ToInt32(number, 2);
+                    if (VariableEngine.GetPool("/").HasVariableWithName("var" + idx))
+                    {
+                        Variable v = VariableEngine.Get("/var" + idx);
+                        bool ok; byte bt;
+                        while(!(ok = byte.TryParse(Console.ReadLine(), out bt)))
+                        {
+                            Console.WriteLine("[INPUT ERROR: PLEASE ENTER value BETWEEN 0 and 255]");
+                        }
+                        v.Value = (int)bt;
+                    }
+                    else
+                    {
+                        Console.WriteLine("[NOT DECLARED: 0x" + number + "]");
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ KLanguage/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infinity.Grammar;
+using Infinity.Grammar.Utility;
+using Infinity.Utility;
+using Infinity.Scripting.IGS;
+using Infinity.Grammar.Parsing;
+
+namespace KLanguage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0) Console.WriteLine("KLanguage ?filename.k -- runs a K Language file\nKLanguage -manual -- Displays manual");
+            else if (args[0] == "-manual")
+            {
+                string[] lines = Properties.Resources.KGrammar.Split(new char[] { '\n', '\r'}, StringSplitOptions.RemoveEmptyEntries);
+                lines = lines.Select((x) => x.Replace("GrammarElement", "")
+                                             .Replace("SymbolSet", "")
+                                             .Replace("Symbol", "")
+                                             .Replace("<C>", "")
+                                             .Replace("<M>", "")
+                                             .Replace("<R>", "")
+                                             .Trim()
+                                             ).ToArray();
+                Console.WriteLine("READ THE GRAMMAR PLEASE\n\n");
+                foreach (string s in lines)
+                    Console.Write(s.Trim() + "\n");
+            }
+            else
+            {
+                Infinity.Engine.Runtime.RuntimeAssemblyManager.InitAssemblies();
+                Grammar kgrammar = new Grammar();
+                InfinityGrammarScriptEngine.LoadGrammar(kgrammar, Properties.Resources.KGrammar);
+                FileInfo input = new FileInfo(args[0]);
+                if (input.Exists)
+                {
+                    string code = input.OpenText().ReadToEnd();
+                    string backup = code;
+                    TestResult parse = kgrammar.Parse(code, true);
+                    if (parse.Result)
+                    {
+                        new KEnv().Interpret((ParseTreeNode)parse.Data["$PARSETREENODE$"]);
+                    }
+                }
+                Console.WriteLine("[INTERPRETATION COMPLETE: PRESS ANY KEY TO EXIT]");
+                Console.ReadKey();
+            }
+        }
+    }
+}
